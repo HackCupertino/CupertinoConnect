599fc385da3f6fd63c743cbb7ee8d8bd433f6088
Pratha B <prathabodas@gmail.com>
2017-04-09 04:26:41 -0700

    notifs

--- app/src/main/java/com/example/prathabodas/cupertinoconnect/MainActivity.java
+++ app/src/main/java/com/example/prathabodas/cupertinoconnect/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.prathabodas.cupertinoconnect;
 
 import android.Manifest;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -62,7 +64,12 @@ public class MainActivity extends AppCompatActivity {
             Intent resultIntent = new Intent(this, MainActivity.class);
             TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
             stackBuilder.addParentStack(MainActivity.class);
-
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+            mBuilder.setContentIntent(resultPendingIntent);
+            int mynotif = 1;
+            NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.notify(mynotif, mBuilder.build());
         }
     }
 
