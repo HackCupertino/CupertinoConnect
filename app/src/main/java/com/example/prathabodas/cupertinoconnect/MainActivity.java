70cf68757e8a1b66673be4c030b488052f0f2e25
Pratha B <prathabodas@gmail.com>
2017-04-09 03:36:19 -0700

    new distance formula implementation

--- app/src/main/java/com/example/prathabodas/cupertinoconnect/MainActivity.java
+++ app/src/main/java/com/example/prathabodas/cupertinoconnect/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.prathabodas.cupertinoconnect;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button viewLoc = (Button)findViewById(R.id.viewLoc);
+        Button viewLoc = (Button) findViewById(R.id.viewLoc);
         viewLoc.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent transition = new Intent(MainActivity.this, MapActivity.class);
@@ -27,6 +35,27 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(transition);
             }
         });
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+
+        double distanceforminsqrt = Math.pow(longitude - 37.3258,2) + Math.pow(latitude - 122.0424, 2);
+
+        if (Math.sqrt(distanceforminsqrt) <= 0.0728)
+            System.out.println("send notification");
     }
 
 }
