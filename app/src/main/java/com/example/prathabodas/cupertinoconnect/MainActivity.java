b04e270459dbde8da6a90bff47dd708d20acfbc5
Pratha B <prathabodas@gmail.com>
2017-04-09 03:52:32 -0700

    maybe works for notifs w/o firebase

--- app/src/main/java/com/example/prathabodas/cupertinoconnect/MainActivity.java
+++ app/src/main/java/com/example/prathabodas/cupertinoconnect/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.prathabodas.cupertinoconnect;
 
 import android.Manifest;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -9,6 +10,7 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 
@@ -54,8 +56,15 @@ public class MainActivity extends AppCompatActivity {
 
         double distanceforminsqrt = Math.pow(longitude - 37.3258,2) + Math.pow(latitude - 122.0424, 2);
 
-        if (Math.sqrt(distanceforminsqrt) <= 0.0728)
-            System.out.println("send notification");
+        if (Math.sqrt(distanceforminsqrt) <= 0.0728) {//&& the button is pressed)
+            android.support.v4.app.NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                           // .setSmallIcon(R.drawable.notification_icon)
+                            .setContentTitle("Cupertino Connect")
+                            .setContentText("Would you like to hang out at the Quinlan Community Center?");
+            Intent resultIntent = new Intent(this, MainActivity.class);
+
+        }
     }
 
 }
