37dcb948a5a8e730770d2535e5253ec44b8c3b26
oleonh <oleon.bball@gmail.com>
2017-04-08 23:18:36 -0700

    map stuff

--- app/src/main/java/com/example/prathabodas/cupertinoconnect/MapActivity.java
+++ app/src/main/java/com/example/prathabodas/cupertinoconnect/MapActivity.java
@@ -1,22 +1,39 @@
 package com.example.prathabodas.cupertinoconnect;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 
-import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
 
 /**
  * Created by prathabodas on 4/8/17.
  */
 
 public class MapActivity extends Activity {
-   // @Override
+    // @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         Intent intent = getIntent();
-    }
 
+        // Create a Uri from an intent string. Use the result to create an Intent.
+        Uri gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
+
+        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        // Make the Intent explicit by setting the Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        // Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
+
+
+    }
 
 }
