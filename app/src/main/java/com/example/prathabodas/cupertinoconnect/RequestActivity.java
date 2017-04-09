caf800bcd1c64f37a025e8598599d5a0bf70f6df
oleonh <oleon.bball@gmail.com>
2017-04-09 05:10:39 -0700

    Changed ListAdapter...
    …and added a MEME

--- app/src/main/java/com/example/prathabodas/cupertinoconnect/RequestActivity.java
+++ app/src/main/java/com/example/prathabodas/cupertinoconnect/RequestActivity.java
@@ -28,20 +28,20 @@ public class RequestActivity extends ListActivity {
         Intent intent = getIntent();
 
         ArrayList<EventInfo> test = new ArrayList<>();
-        test.add(new EventInfo("Quinlan Center", new GregorianCalendar(2017, 1, 1)));
-        test.add(new EventInfo("Quinlan Centerr", new GregorianCalendar(2017, 1, 2)));
-        test.add(new EventInfo("Quinlan Centerrr", new GregorianCalendar(2017, 1, 3)));
-        test.add(new EventInfo("Quinlan Centerrrrr", new GregorianCalendar(2017, 1, 4)));
-        test.add(new EventInfo("Quinlan Centerrrrrr", new GregorianCalendar(2017, 1, 5)));
-        test.add(new EventInfo("Quinlan Centerrrrrrr", new GregorianCalendar(2017, 1, 6)));
-        test.add(new EventInfo("Quinlan Centerrrrrrrr", new GregorianCalendar(2017, 1, 7)));
-        test.add(new EventInfo("Quinlan Centerrrrrrrrr", new GregorianCalendar(2017, 1, 8)));
-        test.add(new EventInfo("Quinlan Centerrrrrrrrrr", new GregorianCalendar(2017, 1, 9)));
-        test.add(new EventInfo("Quinlan Centerrrrrrrrrrr", new GregorianCalendar(2017, 1, 10)));
-        test.add(new EventInfo("Quinlan Centerrrrrrrrrrrr", new GregorianCalendar(2017, 1, 11)));
-        test.add(new EventInfo("Quinlan Centerrrrrrrrrrrrr", new GregorianCalendar(2017, 1, 12)));
-        test.add(new EventInfo("Quinlan Centerrrrrrrrrrrrrr", new GregorianCalendar(2017, 1, 13)));
-        test.add(new EventInfo("Quinlan Centerrrrrrrrrrrrrrr", new GregorianCalendar(2017, 1, 14)));
+        test.add(new EventInfo("Quinlan Center", new MyGregorianCalendar(2017, 1, 1)));
+        test.add(new EventInfo("Quinlan Centerr", new MyGregorianCalendar(2017, 1, 2)));
+        test.add(new EventInfo("Quinlan Centerrr", new MyGregorianCalendar(2017, 1, 3)));
+        test.add(new EventInfo("Quinlan Centerrrrr", new MyGregorianCalendar(2017, 1, 4)));
+        test.add(new EventInfo("Quinlan Centerrrrrr", new MyGregorianCalendar(2017, 1, 5)));
+        test.add(new EventInfo("Quinlan Centerrrrrrr", new MyGregorianCalendar(2017, 1, 6)));
+        test.add(new EventInfo("Quinlan Centerrrrrrrr", new MyGregorianCalendar(2017, 1, 7)));
+        test.add(new EventInfo("Quinlan Centerrrrrrrrr", new MyGregorianCalendar(2017, 1, 8)));
+        test.add(new EventInfo("Quinlan Centerrrrrrrrrr", new MyGregorianCalendar(2017, 1, 9)));
+        test.add(new EventInfo("Quinlan Centerrrrrrrrrrr", new MyGregorianCalendar(2017, 1, 10)));
+        test.add(new EventInfo("Quinlan Centerrrrrrrrrrrr", new MyGregorianCalendar(2017, 1, 11)));
+        test.add(new EventInfo("Quinlan Centerrrrrrrrrrrrr", new MyGregorianCalendar(2017, 1, 12)));
+        test.add(new EventInfo("Quinlan Centerrrrrrrrrrrrrr", new MyGregorianCalendar(2017, 1, 13)));
+        test.add(new EventInfo("Quinlan Centerrrrrrrrrrrrrrr", new MyGregorianCalendar(2017, 1, 14)));
 
 
         setListAdapter(new ArrayAdapter<>(RequestActivity.this, android.R.layout.simple_list_item_1, test));
@@ -51,10 +51,10 @@ public class RequestActivity extends ListActivity {
 
 class EventInfo {
     private String eventLocation;
-    private GregorianCalendar date;
+    private MyGregorianCalendar date;
     private boolean accepted = false, declined = false;
 
-    public EventInfo(String eventLocation, GregorianCalendar date) {
+    public EventInfo(String eventLocation, MyGregorianCalendar date) {
         this.eventLocation = eventLocation;
         this.date = date;
     }
@@ -62,5 +62,21 @@ class EventInfo {
     @Override
     public String toString() {
         return "Location: " + eventLocation + "\r\nDate: " + date;
+    }
+}
+
+class MyGregorianCalendar extends GregorianCalendar {
+    private int year, month, dayOfMonth;
+
+    public MyGregorianCalendar(int year, int month, int dayOfMonth) {
+        super(year, month, dayOfMonth);
+        this.year = year;
+        this.month = month;
+        this.dayOfMonth = dayOfMonth;
+    }
+
+    @Override
+    public String toString() {
+        return dayOfMonth + "/" + month + "/" + year;
     }
 }
