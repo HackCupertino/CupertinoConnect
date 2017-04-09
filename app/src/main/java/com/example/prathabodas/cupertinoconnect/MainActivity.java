62bbe8a8d2f466f8613e85211aaed4c5e672b629
Pratha B <prathabodas@gmail.com>
2017-04-08 22:29:36 -0700

    Initial commit WOOT WOOT

---
+++ app/src/main/java/com/example/prathabodas/cupertinoconnect/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.prathabodas.cupertinoconnect;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button go = (Button)findViewById(R.id.go);
+        go.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent transition = new Intent(MainActivity.this, MapActivity.class);
+                MainActivity.this.startActivity(transition);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/prathabodas/cupertinoconnect/MapActivity.java
@@ -0,0 +1,21 @@
+package com.example.prathabodas.cupertinoconnect;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.MapView;
+
+/**
+ * Created by prathabodas on 4/8/17.
+ */
+
+public class MapActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        Intent intent = getIntent();
+        //MapView map = new MapView()
+    }
+}
