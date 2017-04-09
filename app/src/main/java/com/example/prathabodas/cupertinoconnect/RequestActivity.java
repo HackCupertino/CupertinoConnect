91f5d33795dbe3b3ffcda5f2b37f3dde5f7162ca
oleonh <oleon.bball@gmail.com>
2017-04-09 06:41:28 -0700

    Added placeholder "ignore" button

--- app/src/main/java/com/example/prathabodas/cupertinoconnect/RequestActivity.java
+++ app/src/main/java/com/example/prathabodas/cupertinoconnect/RequestActivity.java
@@ -9,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -86,6 +87,10 @@ class EventLayout extends TableLayout {
                 dateRow.addView(dateText);
             eventDetails.addView(dateRow);
         addView(eventDetails);
+
+        Button ignoreButton = new Button(context);
+        ignoreButton.setText("Ignore");
+        addView(ignoreButton);
     }
 
 }
